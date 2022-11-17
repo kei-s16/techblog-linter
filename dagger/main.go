66c3fe4416fdf9afa1f7b1ff79af7cb8b9afd6b0
kei-s16 <kei1216s@nifty.com>
2022-11-17 23:06:10 +0900

    daggerの実行コンテナにプロジェクトディレクトリをマウント

--- dagger/main.go
+++ dagger/main.go
@@ -16,19 +16,26 @@ func main() {
 
 func lint(ctx context.Context) error {
 	// daggerクライアントの初期化
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout), dagger.WithWorkdir("../"))
 	if err != nil {
 		return err
 	}
 	defer client.Close()
 
+	// workdirのパスを取得
+	projectRoot := client.Host().Workdir()
+
 	// `node` コンテナイメージを持ってくる
 	node := client.Container().From("node:18.11.0")
 
-	// `node` コンテナでnodeのバージョンを出す
-	node = node.Exec(dagger.ContainerExecOpts{
-		Args: []string{"node", "-v"},
-	})
+	// `node` コンテナの/srcにworkdirをマウントする
+	node = node.WithMountedDirectory("/src", projectRoot).WithWorkdir("/src")
+
+	// `node` コンテナでworkdirをls(これは消す)
+	node = node.
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{"ls"},
+		})
 
 	if _, err := node.ExitCode(ctx); err != nil {
 		panic(err)
